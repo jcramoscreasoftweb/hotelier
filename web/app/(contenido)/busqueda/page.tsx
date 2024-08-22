722659e32a7c33eb02da9bb77ec2311e01d2bee4
Respinoza2704 <respinoza@creasoftweb.com>
2024-08-22 15:38:06 -0500

    Update buscardor de habitaciones/ Modal detalle habitacion

--- web/app/(contenido)/busqueda/page.tsx
+++ web/app/(contenido)/busqueda/page.tsx
@@ -16,7 +16,69 @@ export default function Busqueda() {
         <section className="ui_seccion_1_busqueda">
           <div className="ui_marco_opciones_home no-position">
               <div className="ui_barra_busqueda">
+                <form className="ui_form_busqueda" action="" method="get">
+                  <div className="item_input_form">
+                    <div className="label">
+                      <Image width={18} height={20} src="/img/icon_date.png" alt="icon-search"></Image>
+                      <h2>Fecha de entrada</h2>
+                    </div>
+                    <input type="date" name="checkin" id="checkin"/>
+                  </div>
 
+                  <div className="ui_separator_input"></div>
+
+                  <div className="item_input_form">
+                    <div className="label">
+                      <Image width={18} height={20} src="/img/icon_date.png" alt="icon-search"></Image>
+                      <h2>Fecha de salida</h2>
+                    </div>
+                    <input type="date" name="checkout" id="checkout" />
+                  </div>
+
+                  <div className="ui_separator_input"></div>
+
+                  <div className="item_input_form">
+                    <div className="label">
+                      <Image width={18} height={20} src="/img/icon_huesped.png" alt="icon-search"></Image>
+                      <h2>Huéspedes</h2>
+                    </div>
+                    <h2 className="label_huespedes">1 adulto</h2>
+
+                    <div className="ui_box_huespedes">
+
+                      <div className="item_box_huesped">
+                        <h2>Adultos</h2>
+                        <div className="contador_huesped">
+                          <button type="button" className="decrement" data-target="adults">−</button>
+                          <input type="text" id="adults" name="adults" value="1" min="1" max="10" readOnly />
+                          <button type="button" className="increment" data-target="adults">+</button>
+                        </div>
+                      </div>
+
+                      <div className="item_box_huesped">
+                        <h2>Niños</h2>
+                        <div className="contador_huesped">
+                          <button type="button" className="decrement" data-target="children">−</button>
+                          <input type="text" id="children" name="children" value="0" min="0" max="10" readOnly />
+                          <button type="button" className="increment" data-target="children">+</button>
+                        </div>
+                      </div>
+                      
+                      <div className="btn_huesped">Aplicar</div>
+                    
+                    </div>
+
+
+                  </div>
+                  
+                  
+
+                  <button className="btn_buscar" >
+                    <Image width={26} height={26} src="/img/icon_buscar.png" alt="icon-buscar"></Image>
+                    Buscar
+                  </button>
+                </form>
+                
               </div>
           </div>
         </section>
@@ -34,11 +96,106 @@ export default function Busqueda() {
 
       </div>
 
+    {/* <!-- MODAL DETALLE --> */}
+    <div className="ui_fondo_modal ui_detalle_reserva">
+            <div className="ui_marco_modal">
+                <div className="ui_contenido_modal">
+                    <div className="ui_close_modal"></div>
+                    <div className="ui_modal_imagenes_habitacion">
+                      <h2>Habitación Doble Estandar</h2>
 
-      
+                        <div className="ui_carrusel_habitacion_modal">
+                          <div className="item_carrusel">
+                            <Image width={430} height={250} src="/img/room_1.png" alt="image-habitacion" />
+                          </div>
+                          <div className="item_carrusel">
+                            <Image width={430} height={250} src="/img/room_1.png" alt="image-habitacion" />
+                          </div>
+                          <div className="item_carrusel">
+                            <Image width={430} height={250} src="/img/room_1.png" alt="image-habitacion" />
+                          </div>
+                          <div className="item_carrusel">
+                            <Image width={430} height={250} src="/img/room_1.png" alt="image-habitacion" />
+                          </div>
+                        </div>
 
-      
+                        <div className="ui_mini_carrusel">
+                          <div className="item_carrusel">
+                            <Image width={120} height={90} src="/img/room_1.png" alt="image-habitacion" />
+                          </div>
+                          <div className="item_carrusel">
+                            <Image width={120} height={90} src="/img/room_1.png" alt="image-habitacion" />
+                          </div>
+                          <div className="item_carrusel">
+                            <Image width={120} height={90} src="/img/room_1.png" alt="image-habitacion" />
+                          </div>
+                          <div className="item_carrusel">
+                            <Image width={120} height={90} src="/img/room_1.png" alt="image-habitacion" />
+                          </div>
+                        </div>
+
+                      
+                      
+                    </div>
+                    <div className="ui_modal_informacion_habitacion">
+
+                      <div className="row_detalle">
+                        <div className="col_detalle">
+                          <div className="item_detalle">
+                            <Image width={20} height={20} src="/img/icon_hora.png" alt="icon-habitacion"/>
+                            <h2>Check in: 09:00 a.m</h2>
+                          </div>
+
+                          <div className="item_detalle">
+                            <Image width={20} height={20} src="/img/icon_area.png" alt="icon-habitacion"/>
+                            <h2>20 m²</h2>
+                          </div>
+
+                          <div className="item_detalle">
+                            <Image width={20} height={20} src="/img/icon_bed.png" alt="icon-habitacion"/>
+                            <h2>1 cama doble</h2>
+                          </div>
+                        </div>
+
+                        <div className="col_detalle">
+                          <div className="item_detalle">
+                            <Image width={20} height={20} src="/img/icon_hora.png" alt="icon-habitacion"/>
+                            <h2>Check out: 12:00 p.m</h2>
+                          </div>
+
+                          <div className="item_detalle">
+                            <Image width={20} height={20} src="/img/icon_huesped.png" alt="icon-habitacion"/>
+                            <h2>2 adultos</h2>
+                          </div>
+                        </div>
+                      </div>
+
+                      <p>Disfrute de una estancia cómoda y relajante en nuestra habitación doble estándar con dos camas. Esta habitación está diseñada para ofrecer el máximo confort y conveniencia, ideal para amigos, familiares o compañeros de trabajo que viajan juntos.</p>
+
+                      <div className="ui_servicios_politicas">
+                        <span>Servicios:</span>
+                        <ul>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                          <li>Balcon</li>
+                        </ul>
+                      </div>
+                    </div>
+                </div>
+
+            </div>
+    </div>
+    {/* <!-- END MODAL DETALLE--> */}
 
     </>
   )
 }
+
--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 
+/*****  HOME ******/
 $(".ui_carousel_beneficios").slick({
     infinite: true,
     slidesToShow: 3,
@@ -8,14 +9,7 @@ $(".ui_carousel_beneficios").slick({
     prevArrow:'<span class="btn-siguiente"></span>'
 
 })
-
-$(".carousel_fotos_habitacion").slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow:'<span class="btn-siguiente"></span>',
-    prevArrow:'<span class="btn-anterior"></span>'
-})
+/*****  END HOME ******/
 
 /*****  GALERIA ******/
 $(".ui_carrusel_item_galeria").slick({
@@ -32,35 +26,107 @@ $(".ui_carrusel_item_galeria .ui_item_slider img").click(function(){
 })
 /***** END GALERIA ******/
 
-$(".ui_info_habitacion_precio .ui_item_precio").click(function(){
 
-    $(".ui_info_habitacion_precio .ui_item_precio").removeClass("activo")
-    $(".ui_btn_item_reservar_habitacion").removeClass("activo")
-
-    $(this).addClass("activo")
-    $(this).parent().find(".ui_btn_item_reservar_habitacion").addClass("activo")
-
-})
-
+/***** RESEÑA ******/
 $(".ui_btn_paginacion .ui_item_btn_page").click(function(){
     $(".ui_btn_paginacion .ui_item_btn_page").removeClass("activo")
     $(this).addClass("activo")
 })
 
+/***** END RESEÑA ******/
+
+
+
+/*****  PAGE BUSQUEDA ******/
+$(".carousel_fotos_habitacion").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow:'<span class="btn-siguiente"></span>',
+    prevArrow:'<span class="btn-anterior"></span>'
+})
+
+$(".ui_mini_carrusel").slick({
+    arrows:false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    asNavFor: '.ui_carrusel_habitacion_modal',
+})
+
+$(".ui_carrusel_habitacion_modal").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow:'<span class="btn-siguiente"></span>',
+    prevArrow:'<span class="btn-anterior"></span>',
+    asNavFor: '.ui_mini_carrusel',  
+})
+
+
+
+$(".ui_info_habitacion_precio .ui_item_precio").click(function(){
+    $(".ui_info_habitacion_precio .ui_item_precio").removeClass("activo")
+    $(".ui_btn_item_reservar_habitacion").removeClass("activo")
+
+    $(this).addClass("activo")
+    $(this).parent().find(".ui_btn_item_reservar_habitacion").addClass("activo")
+})
+
 $(".ui_listado_habitaciones .ui_info_habitacion .ui_ver_mas").click(function (){
-$(".ui_fondo_modal.ui_detalle_reserva").fadeIn();
+    $(".ui_fondo_modal.ui_detalle_reserva").fadeIn();
 })
 
 $(".ui_fondo_modal .ui_close_modal").click(function(){
     $(".ui_fondo_modal.ui_detalle_reserva").fadeOut();
 })
 
-$(window).click(function(event){
-  // determina el elemento en el cual se ejecuto el clcik y si es el elemento fuera del contenido de modal se cierra modal
-    if($(event.target).hasClass("ui_marco_modal")){
-        $(".ui_fondo_modal.ui_detalle_reserva").fadeOut();
+// $(window).click(function(event){
+//   // determina el elemento en el cual se ejecuto el clcik y si es el elemento fuera del contenido de modal se cierra modal
+//     if($(event.target).hasClass("ui_marco_modal")){
+//         $(".ui_fondo_modal.ui_detalle_reserva").fadeOut();
+//     }
+// })
+
+/*****  END PAGE BUSQUEDA ******/
+
+
+/***** BUSCADOR HABITACIONES ******/
+$(".label_huespedes").click(function() {
+    $(".ui_box_huespedes").fadeIn(300)
+});
+$(".increment").click(function() {
+    let target = $(this).data("target");
+    let $input = $("#" + target);
+    let value = parseInt($input.val());
+    let max = parseInt($input.attr("max"));
+    if (value < max) {
+        $input.val(value + 1);
     }
-})
+});
+$(".decrement").click(function() {
+    let target = $(this).data("target");
+    let $input = $("#" + target);
+    let value = parseInt($input.val());
+    let min = parseInt($input.attr("min"));
+    if (value > min) {
+        $input.val(value - 1);
+    }
+});
+$(".ui_box_huespedes .btn_huesped").click(function() {
+    let adults = parseInt($("#adults").val());
+    let children = parseInt($("#children").val());
+    let labelText = adults + " adulto" + (adults > 1 ? "s" : "");
+    if (children > 0) {
+        labelText += " y " + children + " niño" + (children > 1 ? "s" : "");
+    }
+    $(".label_huespedes").text(labelText);
+    $(".ui_box_huespedes").fadeOut(300);
+});
+
+/***** END BUSCADOR HABITACIONES ******/
+
+
 
 
 
