d30a1c2b7186613fca4593137c112ace54e91ba9
unknown <jtorriccelli1986@gmail.com>
2024-11-21 11:27:58 -0500

    update

---
+++ web/components/busqueda-habitaciones/busquedaHabitaciones.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { useEffect, useState } from "react";
+import { ItemHabitacionBusqueda } from "../busqueda/itemHabitacionBusqueda";
+import { useSearchParams } from 'next/navigation'
+
+export const BusquedaHabitaciones=({contenidoBusqueda}:any)=>{
+    const [dataHabitaciones, setDataHabitaciones] = useState(null);        // Estado para almacenar los datos
+    const [loading, setLoading] = useState(true);   // Estado para el indicador de carga
+    const [error, setError] = useState(null);
+    const [records, setrecords] = useState(false);
+
+
+    const searchParams = useSearchParams()
+
+    useEffect(() => {
+        // Función para obtener los datos de la API
+        const fetchData = async () => {
+            try {
+                const username = "#|~4139h*II7yN@";
+                const password = "r)qFV.Ey8W6PX5>";
+
+                // Codifica las credenciales en Base64
+                const auth = btoa(`${username}:${password}`);
+                const response = await fetch(`http://18.222.202.240/api/search-typerooms?${searchParams.toString()}`,{
+                    method: "GET", // GET es el valor predeterminado
+                    headers: {
+                        Authorization: `Basic ${auth}`, // Encabezado de autenticación
+                        "Content-Type": "application/json" // Opcional, depende del servicio
+                    }
+                } );
+                if (!response.ok) throw new Error('Error en la solicitud');
+
+                const result = await response.json();
+
+                setrecords(result.records)
+                setDataHabitaciones(result.payload["es"]); // Asigna los datos obtenidos al estado
+            } catch (error:any) {
+                setError(error.message);       // Asigna el mensaje de error al estado
+            } finally {
+                setLoading(false);             // Finaliza el indicador de carga
+            }
+        };
+
+        fetchData(); // Llama a la función de fetch
+    }, []); // Dependencia vacía para ejecutar solo en el montaje
+
+    if (loading) return <div></div>; // Indicador de carga
+    if (error) return <div></div>;
+    if(!records) return <>  <section className="ui_mensaje_habitaciones_no_encontradas">
+                            <h2>No se encontaron habitaciones con los valores seleccionados</h2>
+                            </section></> ;          // Mensaje de error
+
+    return (
+        <>
+
+        {dataHabitaciones.map((item:any)=> {
+             return(
+                <ItemHabitacionBusqueda key={item.id_tp_room} item={item} contenidoBusqueda={contenidoBusqueda}></ItemHabitacionBusqueda>
+            );
+
+
+        })}
+      </>
+
+
+    );
+
+
+
+    /*let url = "https://creasoft.com.pe/hotelier_api/pages/search-typerooms.json?V4";
+    let contenido = await fetch(url);
+    let data = await contenido.json();
+    let tipoHabitacion: BusquedaResponse[] = data.payload["es"];*/
+
+}
+
+const ListadoBusqueda= ()=>{
+return (<></>)
+}
--- web/components/busqueda-habitaciones/componentes.tsx
+++ web/components/busqueda-habitaciones/componentes.tsx
@@ -1,29 +1,66 @@
 'use client'
+import { useEffect, useState } from "react";
 import { useSearchParams } from 'next/navigation'
 import { HabitacionesNoDisponible, ListadoHabitaciones } from './listadoHabitaciones';
 
 export const ContenidoBusquedaHabitacion =  () => {
 
+    const [data, setData] = useState(null);        // Estado para almacenar los datos
+    const [loading, setLoading] = useState(true);   // Estado para el indicador de carga
+    const [error, setError] = useState(null);
+
     const searchParams = useSearchParams()
 
     const adults = searchParams.get('adults')
     const children = searchParams.get('children')
-    const date_in = searchParams.get('checkin')
-    const date_out = searchParams.get('checkout')
+    const date_in = searchParams.get('date_in')
+    const date_out = searchParams.get('date_out')
+    console.log(date_in.length)
+    console.log(date_out)
 
-
+    /*console.log(typeof (adults));
+    console.log(children);
+    console.log(date_in);
+    console.log(date_out);*/
     let estado_parametros=false
-    if(adults=="0"  || children=="0" || date_in?.length==0 || date_out?.length==0){
-
+    if(adults=="0" ||  date_in?.length==0 || date_out?.length==0){
+        console.log("*********************")
     }else{
+
         estado_parametros=true;
 
     }
 
+
+    useEffect(() => {
+        // Función para obtener los datos de la API
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://creasoft.com.pe/hotelier_api/pages/page-busqueda.json?v56");
+                if (!response.ok) throw new Error('Error en la solicitud');
+                const result = await response.json();
+                setData(result.payload["es"]); // Asigna los datos obtenidos al estado
+            } catch (error:any) {
+                setError(error?.message);       // Asigna el mensaje de error al estado
+            } finally {
+                setLoading(false);             // Finaliza el indicador de carga
+            }
+        };
+
+        fetchData(); // Llama a la función de fetch
+    }, []);
+
+    //if (loading) return <<div>Cargando habitaciones...</div>>; // Indicador de carga
+    //if (error) return <div>Error: {error}</div>;             // Mensaje de error*/
+    if (loading) return <></>; // Indicador de carga
+    if (error) return <></>;             // Mensaje de error*/
+
+
+
     return (
         <>
 
-            {estado_parametros ? <ListadoHabitaciones /> : <HabitacionesNoDisponible />}
+            {estado_parametros ? <ListadoHabitaciones contenidoBusqueda={data}/> : <HabitacionesNoDisponible contenidoBusqueda={data}/>}
 
         </>
     )
